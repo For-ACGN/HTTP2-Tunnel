48bd6356553f0c2f42cc2fe8c5c571f3e7765bee
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-04 14:00:11 +0800

    implement Listener.Accept, add utlsConn and hook Read and Write.

--- listener.go
+++ listener.go
@@ -1,8 +1,10 @@
 package msocks
 
 import (
+	"crypto/sha256"
 	"crypto/tls"
 	"net"
+	"sync"
 
 	"github.com/For-ACGN/utls"
 )
@@ -10,7 +12,7 @@ import (
 var tlsNextProtos = []string{"h2", "http/1.1"}
 
 type utlsListener struct {
-	listener net.Listener
+	net.Listener
 
 	config *utls.Config
 	secret []byte
@@ -37,20 +39,70 @@ func newUTLSListener(listener net.Listener, config *tls.Config, secret []byte) *
 	}
 	cfg.NextProtos = tlsNextProtos
 	return &utlsListener{
-		listener: listener,
+		Listener: listener,
 		config:   cfg,
 		secret:   secret,
 	}
 }
 
 func (ul *utlsListener) Accept() (net.Conn, error) {
-	conn, err := ul.listener.Accept()
+	conn, err := ul.Listener.Accept()
 	if err != nil {
 		return nil, err
 	}
-	return &utlsConn{Conn: conn}, nil
+	uc := &utlsConn{}
+	cfg := ul.config.Clone()
+	cfg.OnClientHelloMessage = func(hello *utls.ClientHelloMessage) error {
+		h := sha256.New()
+		h.Write(hello.Random)
+		h.Write(ul.secret)
+		if isCovertDigest(h.Sum(nil)) {
+			uc.covert = true
+		}
+		return nil
+	}
+	uc.Conn = utls.Server(conn, cfg)
+	return uc, nil
 }
 
 type utlsConn struct {
-	net.Conn
+	*utls.Conn
+
+	covert bool
+
+	rmu sync.Mutex
+	wmu sync.Mutex
+}
+
+func (uc *utlsConn) Read(b []byte) (int, error) {
+	err := uc.Conn.Handshake()
+	if err != nil {
+		return 0, err
+	}
+	uc.rmu.Lock()
+	defer uc.rmu.Unlock()
+	if uc.covert {
+		// TODO simulate http2 behavior
+		uc.covert = false
+	}
+	return uc.Conn.Read(b)
+}
+
+func (uc *utlsConn) Write(b []byte) (int, error) {
+	err := uc.Conn.Handshake()
+	if err != nil {
+		return 0, err
+	}
+	uc.wmu.Lock()
+	defer uc.wmu.Unlock()
+	if uc.covert {
+		// TODO simulate http2 behavior
+		uc.covert = false
+	}
+	return uc.Conn.Write(b)
+}
+
+// check the first 18 bit are all zero.
+func isCovertDigest(digest []byte) bool {
+	return digest[0] == 0x00 && digest[1] == 0x00 && digest[2]>>6 == 0x00
 }
