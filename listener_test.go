a401394afb157d088e5441567cf96649553ee466
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-04 14:13:42 +0800

    add TestCovertDigest for adjust the number of the prefix bit.

--- listener_test.go
+++ listener_test.go
@@ -1,1 +1,26 @@
 package msocks
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestCovertDigest(t *testing.T) {
+	rd := rand.New(rand.NewSource(time.Now().Unix()))
+	buf := make([]byte, 32)
+	h := sha256.New()
+	for i := 0; i < 10000000; i++ {
+		rd.Read(buf)
+		h.Write(buf)
+		h.Write(buf)
+		digest := h.Sum(nil)
+		if isCovertDigest(digest) {
+			fmt.Println(hex.EncodeToString(digest))
+		}
+		h.Reset()
+	}
+}
