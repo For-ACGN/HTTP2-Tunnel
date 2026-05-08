a4c14d2e8889e6c656047aacec4853444f2f49e0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-08 16:03:28 +0800

    update http2 simulation about preface and setting frame.

--- http2.go
+++ http2.go
@@ -1,13 +1,47 @@
 package msocks
 
 import (
+	"bytes"
+	"io"
 	"net"
+
+	"golang.org/x/net/http2"
 )
 
-func simulateHTTP2Client(conn net.Conn) error {
+const (
+	settingFrameSize = 46
+)
+
+func simulateHTTP2Client(conn net.Conn, preface []byte) error {
+	// rand := newMathRand()
+
+	if len(preface) == 0 {
+		preface = []byte(http2.ClientPreface)
+	}
+
+	// write preface and setting frame
+	buf := bytes.NewBuffer(make([]byte, 0, len(preface)+64))
+	buf.Write(preface)
+	buf.Write(bytes.Repeat([]byte{0x00}, settingFrameSize))
+	_, err := buf.WriteTo(conn)
+	if err != nil {
+		return err
+	}
+
+	// write the first request with header
+	// buf.Reset()
+	// buf.Write(bytes.Repeat([]byte{0x00}, rand))
+
 	return nil
 }
 
 func simulateHTTP2Server(conn net.Conn) error {
+	size := 0
+	size += len(http2.ClientPreface)
+	size += settingFrameSize
+	_, err := io.CopyN(io.Discard, conn, int64(size))
+	if err != nil {
+		return err
+	}
 	return nil
 }
