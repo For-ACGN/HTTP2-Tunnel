860f7cf110c43b9409a04055223bb35697321d70
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-09 13:32:38 +0800

    add TestBufConn for check the buffered data is correct.

--- buf_conn_test.go
+++ buf_conn_test.go
@@ -1,1 +1,33 @@
 package msocks
+
+import (
+	"bufio"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBufConn(t *testing.T) {
+	testdata := []byte{0x01, 0x02, 0x03, 0x04}
+
+	client, server := net.Pipe()
+
+	go func() {
+		_, err := server.Write(testdata)
+		require.NoError(t, err)
+	}()
+
+	reader := bufio.NewReader(client)
+
+	data, err := reader.Peek(4)
+	require.NoError(t, err)
+	require.Equal(t, testdata, data)
+
+	bConn := newBufConn(client, reader)
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(bConn, buf)
+	require.NoError(t, err)
+	require.Equal(t, testdata, buf)
+}
