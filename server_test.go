30818980390e6d7fc9d1051a80f644273907cbe7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-09 13:34:51 +0800

    add sub test about http/2 for the server http/2 simulation.

--- server_test.go
+++ server_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -65,6 +66,10 @@ func TestServer_Serve(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestServer_handleConn(t *testing.T) {
+
+}
+
 func TestServer_Simulation(t *testing.T) {
 	defer testRemoveServerLogFile(t)
 
@@ -86,6 +91,8 @@ func TestServer_Simulation(t *testing.T) {
 	tlsConfig.RootCAs = x509.NewCertPool()
 	tlsConfig.RootCAs.AddCert(certs[0])
 
+	URL := fmt.Sprintf("https://%s/", address)
+
 	t.Run("http/1.1", func(t *testing.T) {
 		for i := 0; i < 20; i++ {
 			tlsConfig = tlsConfig.Clone()
@@ -99,7 +106,7 @@ func TestServer_Simulation(t *testing.T) {
 			proto := conn.ConnectionState().NegotiatedProtocol
 			require.Equal(t, "http/1.1", proto)
 
-			req, err := http.NewRequest(http.MethodGet, "/", nil)
+			req, err := http.NewRequest(http.MethodGet, URL, nil)
 			require.NoError(t, err)
 			err = req.Write(conn)
 			require.NoError(t, err)
@@ -120,6 +127,28 @@ func TestServer_Simulation(t *testing.T) {
 			tlsConfig = tlsConfig.Clone()
 			tlsConfig.NextProtos = []string{"h2", "http/1.1"}
 
+			req, err := http.NewRequest(http.MethodGet, URL, nil)
+			require.NoError(t, err)
+
+			tr := http.Transport{
+				TLSClientConfig:   tlsConfig,
+				ForceAttemptHTTP2: true,
+			}
+			resp, err := tr.RoundTrip(req)
+			require.NoError(t, err)
+			require.Equal(t, "HTTP/2.0", resp.Proto)
+			_, _ = io.Copy(io.Discard, resp.Body)
+			_ = resp.Body.Close()
+
+			tr.CloseIdleConnections()
+		}
+	})
+
+	t.Run("h2 but h1 request", func(t *testing.T) {
+		for i := 0; i < 20; i++ {
+			tlsConfig = tlsConfig.Clone()
+			tlsConfig.NextProtos = []string{"h2", "http/1.1"}
+
 			conn, err := tls.Dial("tcp", address, tlsConfig)
 			require.NoError(t, err)
 
@@ -128,7 +157,7 @@ func TestServer_Simulation(t *testing.T) {
 			proto := conn.ConnectionState().NegotiatedProtocol
 			require.Equal(t, "h2", proto)
 
-			req, err := http.NewRequest(http.MethodGet, "/", nil)
+			req, err := http.NewRequest(http.MethodGet, URL, nil)
 			require.NoError(t, err)
 			err = req.Write(conn)
 			require.NoError(t, err)
