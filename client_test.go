9260042f66b3a8cccf3291a99ea80bf3ecebc17d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-09 13:57:29 +0800

    replace the config.Server.Address to localhost:2019 for SNI.

--- client_test.go
+++ client_test.go
@@ -33,8 +33,8 @@ func testBuildClientConfig() *ClientConfig {
 	config.Common.LogPath = testClientLogFile
 	config.Common.Password = testPassword
 	config.Client.PreConns = 4
-	config.Server.Network = "tcp"
-	config.Server.Address = "127.0.0.1:2019"
+	config.Server.Network = "tcp4"
+	config.Server.Address = "localhost:2019"
 	config.Server.RootCA = string(ca)
 	config.Front.Network = "tcp"
 	config.Front.Address = "127.0.0.1:2020"
