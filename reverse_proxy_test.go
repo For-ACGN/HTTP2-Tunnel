891c31df4885f20a629d8fbf8823879be45c1ee3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-05-17 13:53:07 +0800

    update TestReverseProxy for add more sub tests about header filter.

--- reverse_proxy_test.go
+++ reverse_proxy_test.go
@@ -17,9 +17,15 @@ func TestReverseProxy(t *testing.T) {
 
 	// start mock backend server
 	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-Backend-Host", r.Host)
-		w.Header().Set("Set-Cookie", "session=abc123")
-		w.Header().Set("Server", "nginx/1.0")
+		header := r.Header
+		require.Empty(t, header.Get("Cookie"))
+		require.Empty(t, header.Get("Authorization"))
+		require.Empty(t, header.Get("Filtered-Header"))
+
+		header = w.Header()
+		header.Set("X-Backend-Host", r.Host)
+		header.Set("Server", "nginx/1.0")
+		header.Set("Set-Cookie", "session=abc123")
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("backend response"))
 	}))
@@ -28,7 +34,7 @@ func TestReverseProxy(t *testing.T) {
 	config := testBuildServerConfig()
 	config.Web.Mode = WebModeProxy
 	config.Web.Proxy.Target = backend.URL
-	config.Web.Proxy.Filter = []string{"filtered_header"}
+	config.Web.Proxy.Filter = []string{"Filtered-Header"}
 	server, err := NewServer(context.Background(), config)
 	require.NoError(t, err)
 
@@ -49,9 +55,10 @@ func TestReverseProxy(t *testing.T) {
 	client := http.Client{
 		Transport: &tr,
 	}
+	URL := "https://127.0.0.1:2019/"
 
 	t.Run("basic proxy", func(t *testing.T) {
-		resp, err := client.Get("https://127.0.0.1:2019/")
+		resp, err := client.Get(URL)
 		require.NoError(t, err)
 		defer func() { _ = resp.Body.Close() }()
 
@@ -61,7 +68,41 @@ func TestReverseProxy(t *testing.T) {
 		require.Equal(t, []byte("backend response"), data)
 	})
 
-	// TODO more sub tests
+	t.Run("request headers stripped", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, URL, nil)
+		require.NoError(t, err)
+		header := req.Header
+		header.Set("Cookie", "secret=token")
+		header.Set("Authorization", "Bearer secret")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		_ = resp.Body.Close()
+	})
+
+	t.Run("strip custom headers", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, URL, nil)
+		require.NoError(t, err)
+		header := req.Header
+		header.Set("filtered_header", "secret=token")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		_ = resp.Body.Close()
+	})
+
+	t.Run("strip response headers", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, URL, nil)
+		require.NoError(t, err)
+		header := req.Header
+		header.Set("Cookie", "secret=token")
+		header.Set("Authorization", "Bearer secret")
+		resp, err := client.Do(req)
+		require.NoError(t, err)
+		defer func() { _ = resp.Body.Close() }()
+
+		header = resp.Header
+		require.Equal(t, "", header.Get("Set-Cookie"))
+		require.Equal(t, "", header.Get("Authorization"))
+	})
 
 	err = server.Close()
 	require.NoError(t, err)
